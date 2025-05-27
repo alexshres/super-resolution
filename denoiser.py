fec59c1de896bb8d23779bf5a889cd4529793e8d
Alex Shrestha <alexshrestha1996@gmail.com>
2025-05-27 10:11:34 -0700

    base unet created

--- denoiser.py
+++ denoiser.py
@@ -3,8 +3,20 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 
 class Denoiser(nn.Module):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(2, 64, 3, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 1, 3, padding=1)
+
+
+    def forward(self, x, t_embed):
+        x = torch.cat([x, t_embed], dim=1)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        return self.conv3(x)
