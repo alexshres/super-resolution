c04d9581b9efc89cf63d9fadb9615846d48a1d63
Alex Shrestha <alexshrestha1996@gmail.com>
2025-05-27 08:57:37 -0700

    starting diffusion super resolution

---
+++ denoiser.py
@@ -0,0 +1,10 @@
+# Alex Shrestha
+# FILE: denoiser.py
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class Denoiser(nn.Module):
+    pass
---
+++ train.py
@@ -0,0 +1,9 @@
+# Alex Shrestha
+# FILE: train.py
+
+import denoiser as d
+
+
+
+def train_loop():
+    pass
